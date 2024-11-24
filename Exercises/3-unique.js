73e175b91d4272b2aee285d73f957179cb760105
Ivan Fedorenko <81463720+fedorenkoivan@users.noreply.github.com>
2024-11-25 00:44:58 +0200

    done all
    done all problems

--- Exercises/3-unique.js
+++ Exercises/3-unique.js
@@ -3,6 +3,14 @@
 // Create and return a new array without duplicate elements
 // Don't modify initial array
 
-const unique = (array) => [];
+const unique = (array) => {
+  const unique = [];
+  array.forEach((elem) => {
+    if (unique.indexOf(elem) === -1) {
+      unique.push(elem);
+    }
+  });
+  return unique || undefined;
+};
 
 module.exports = { unique };
--- Exercises/4-difference.js
+++ Exercises/4-difference.js
@@ -3,6 +3,12 @@
 // Find difference of two arrays
 // elements from array1 that are not includes in array2
 
-const difference = (array1, array2) => [];
+const difference = (array1, array2) => {
+  const diff = [];
+  array1.forEach((elem) => {
+    if (!array2.includes(elem)) diff.push(elem);
+  });
+  return diff;
+};
 
 module.exports = { difference };
